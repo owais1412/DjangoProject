ee0996b3169987669d37367571f7ad21457c74d3
owais1412 <owes.hussain@gmail.com>
2019-04-29 01:04:00 +0530

    Categories Added

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from datetime import datetime
 from tinymce.models import HTMLField
 # Create your models here.
+
+class TutorialCategory(models.Model):
+    category = models.CharField(max_length=200)
+    category_summary = models.CharField(max_length=200)
+    category_slug = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category
+
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
